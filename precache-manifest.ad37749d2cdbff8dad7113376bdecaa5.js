18fea023569bc5d0be6431d389826c8c95275675
Leslie Heinzen <leslie@switcherstudio.com>
2021-04-15 12:34:12 -0400

    Updates

---
+++ precache-manifest.ad37749d2cdbff8dad7113376bdecaa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f29614bb678f3f1213a21e34f55f9b",
+    "url": "/qa-sample/index.html"
+  },
+  {
+    "revision": "39fd6faefd1ac821feba",
+    "url": "/qa-sample/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "148c55c7982e1c37a2c5",
+    "url": "/qa-sample/static/css/main.745c2f58.chunk.css"
+  },
+  {
+    "revision": "39fd6faefd1ac821feba",
+    "url": "/qa-sample/static/js/2.80c07a2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qa-sample/static/js/2.80c07a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148c55c7982e1c37a2c5",
+    "url": "/qa-sample/static/js/main.ccdd9731.chunk.js"
+  },
+  {
+    "revision": "1f154b887da99a1e519c",
+    "url": "/qa-sample/static/js/runtime-main.624cf967.js"
+  }
+]);
